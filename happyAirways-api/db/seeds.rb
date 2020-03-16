6f4f62b256b1b87e76b63251a8f7caa051e1eb82
Erica Hawkins <ehawk901@gmail.com>
2020-03-16 10:02:06 -0500

    Backend

--- happyAirways-api/db/seeds.rb
+++ happyAirways-api/db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+flight_a = Flight.create(origin: "Chicago", destination: "San Francisco")
+flight_b = Flight.create(origin: "Chicago", destination: "Los Angeles")
+flight_c = Flight.create(origin: "Chicago", destination: "Zurich")
+flight_d = Flight.create(origin: "Chicago", destination: "Milan")
+flight_e = Flight.create(origin: "Chicago", destination: "Dallas")
+
+passenger_a = Passenger.create(first_name: "Erica", last_name:"Hawkins")
+passenger_b = Passenger.create(first_name: "Sam", last_name:"West")
+passenger_c = Passenger.create(first_name: "Betty", last_name: "Douglas")
+passenger_d = Passenger.create(first_name: "Amanda", last_name: "Treutler")
+passenger_e = Passenger.create(first_name: "Wayne", last_name: "Brady") 
+
+reservation_a = Reservation.create(flight: flight_a, passenger: passenger_a)
+reservation_b = Reservation.create(flight: flight_b, passenger: passenger_b)
+reservation_c = Reservation.create(flight: flight_c, passenger: passenger_c)
+reservation_d = Reservation.create(flight: flight_d, passenger: passenger_d)
+reservation_e = Reservation.create(flight: flight_e, passenger: passenger_e)
+
