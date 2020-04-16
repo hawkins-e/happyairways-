2fd6964622d182912f2e87790de491226317fc0e
Erica Hawkins <ehawk901@gmail.com>
2020-04-16 15:33:56 -0500

    Switching of database from sql to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_02_24_174713) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "flights", force: :cascade do |t|
     t.string "origin"
     t.string "destination"
