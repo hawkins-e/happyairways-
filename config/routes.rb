83770b6769267c002ca58a8bad720f5b04d15a1e
Erica Hawkins <ehawk901@gmail.com>
2020-04-17 23:39:05 -0500

    Auth Set-Up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  resources :passengers
-  resources :flights
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do 
+    namespace :v1 do
+      resources :passengers
+      resources :flights
+      resources :users, only: [:create]
+      post '/login', to: 'auth#create'
+      get '/profile', to: 'users#profile'
+
+    end
+  end 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_24_174713) do
+ActiveRecord::Schema.define(version: 2020_04_16_220511) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,13 @@ ActiveRecord::Schema.define(version: 2020_02_24_174713) do
     t.datetime "reservation_date"
     t.index ["flight_id"], name: "index_reservations_on_flight_id"
     t.index ["passenger_id"], name: "index_reservations_on_passenger_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "reservations", "flights"
